0894179d7de7c2bf4104857895467c292c7d77b6
SULFI <SULFI@DESKTOP-VJTVQIJ>
2024-10-17 20:49:22 +0530

    Created a sample program

---
+++ samplemaven/src/main/java/samplemavenpackage/SampleMavenPrgm.java
@@ -0,0 +1,10 @@
+package samplemavenpackage;
+
+public class SampleMavenPrgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow maven world");
+	}
+
+}
