c70869910b9a69eafad38fcfbed972be0d28f2a3
SULFI <SULFI@DESKTOP-VJTVQIJ>
2024-10-18 20:39:37 +0530

    Single Inheritance

---
+++ samplemaven/src/main/java/inheritanceprogrms/InheritSingle.java
@@ -0,0 +1,9 @@
+package inheritanceprogrms;
+
+public class InheritSingle {
+public void display1() {
+	
+	System.out.println("This is single inheritance Parent class");
+}
+}
+
---
+++ samplemaven/src/main/java/inheritanceprogrms/SingleChild.java
@@ -0,0 +1,15 @@
+package inheritanceprogrms;
+
+public class SingleChild extends InheritSingle{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	SingleChild obj=new SingleChild();
+	obj.display1();
+	obj.display2();
+	}
+	public void display2() {
+		System.out.println("This is single inheritance child class");
+	}
+
+}
