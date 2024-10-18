f63c291f48a594cb9262515ba91784fa77cdc72a
SULFI <SULFI@DESKTOP-VJTVQIJ>
2024-10-18 16:15:36 +0530

    StringBuilderExample

---
+++ samplemaven/src/main/java/samplemavenpackage/StringBuilderEx1.java
@@ -0,0 +1,19 @@
+package samplemavenpackage;
+
+public class StringBuilderEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb=new StringBuilder("SULFIKER ALI K H");
+		//sb.insert(0, "First");
+		//System.out.println(sb);
+		//sb.append("Java");
+		//System.out.println(sb);
+		//sb.replace(2,3, "XXX");
+		//System.out.println(sb);
+		sb.reverse();
+		System.out.println(sb);
+		
+	}
+
+}
