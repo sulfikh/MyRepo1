79d822d62e3a173fc3c5fc3dd68eeda2c8979db2
SULFI <SULFI@DESKTOP-VJTVQIJ>
2024-10-24 09:59:24 +0530

    Done Encapsulation program

---
+++ samplemaven/src/main/java/encapsulationpgm/Encapschild.java
@@ -0,0 +1,15 @@
+package encapsulationpgm;
+
+public class Encapschild {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encaps1 obj=new Encaps1();
+		obj.setter("Sulfiker",35);
+		obj.getter();
+	}
+
+}
